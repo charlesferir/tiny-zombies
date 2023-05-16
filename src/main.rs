5376b7d5303f1fcaa9d1225eb902d9c514c4501c
Charles Ferir <charlesferir1@gmail.com>
2023-05-16 21:07:04 +0200

    Added vsync toggle

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,46 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PresentMode};
 
 mod map;
+mod player;
 
 use map::MapPugin;
+use player::PlayerPugin;
 
 fn main() {
     App::new()
-        .insert_resource(ClearColor(Color::rgb(
-            118.0 / 255.0,
-            59.0 / 255.0,
-            54.0 / 255.0,
-        )))
-        .init_resource::<map::MapDescriptor>()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Tiny zombies".into(),
+                        // resolution: (500., 300.).into(),
+                        present_mode: PresentMode::AutoNoVsync,
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
         .add_plugin(MapPugin)
+        .add_plugin(PlayerPugin)
         .add_startup_system(setup)
+        .add_system(toggle_vsync)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+fn toggle_vsync(input: Res<Input<KeyCode>>, mut windows: Query<&mut Window>) {
+    if input.just_pressed(KeyCode::V) {
+        let mut window = windows.single_mut();
+
+        window.present_mode = if matches!(window.present_mode, PresentMode::AutoVsync) {
+            PresentMode::AutoNoVsync
+        } else {
+            PresentMode::AutoVsync
+        };
+        info!("PRESENT_MODE: {:?}", window.present_mode);
+    }
 }
